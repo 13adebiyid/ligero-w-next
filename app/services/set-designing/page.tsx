ce6daf0e96bdd99b0b5f19f37b12351f79b54e4d
David <psyda4@nottingham.ac.uk>
2025-08-06 14:27:35 +0100

    1st commit of next ligero website

--- app/services/set-designing/page.tsx
+++ app/services/set-designing/page.tsx
@@ -20,6 +20,7 @@ export default function SetDesigningPage() {
                     </h1>
 
                     <VideoGallery projects={setDesignProjects} />
+
                 </div>
             </div>
         </div>
